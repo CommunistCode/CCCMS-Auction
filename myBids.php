867302f5a1caa86b16348fa6fff5904e7604609b
Jack Mitchell <jack@communistcode.co.uk>
2011-08-11 15:08:08 +0100

    check logged in when going to myBids

--- myBids.php
+++ myBids.php
@@ -3,6 +3,8 @@
 	require_once("../config/config.php");
 	require_once("../includes/global.inc.php");
 
+	require_once($fullPath."/membership/includes/checkLogin.inc.php");
+
 	$heading = "My Bids";
 	$include = "includes/myBidsTable.inc.php";
 
