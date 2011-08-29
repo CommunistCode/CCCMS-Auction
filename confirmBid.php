6dbe5a35a19ac3447de90feb04d429b52ac583a6
Jack Mitchell <jack@communistcode.co.uk>
2011-08-29 09:42:47 +0100

    check for logged in when placing bid

--- confirmBid.php
+++ confirmBid.php
@@ -2,6 +2,7 @@
 
 	require_once("../config/config.php");
 	require_once($fullPath."/includes/global.inc.php");
+	require_once($fullPath."/membership/includes/checkLogin.inc.php");
 
 	$heading = "Confirm Bid";
 	$include = "includes/confirmBid.inc.php";
