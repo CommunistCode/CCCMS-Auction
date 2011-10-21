0446484e9d42e745aa082170339d40adc565ae91
Jack Mitchell <jack@communistcode.co.uk>
2011-10-21 13:23:58 +0100

    only link to seller on finished listings if the item sold

--- includes/finishedListings.inc.php
+++ includes/finishedListings.inc.php
@@ -22,7 +22,19 @@
 			echo("<tr>");	
 			echo("<td><a href='viewListing.php?id=".$finishedListing['id']."'>".$finishedListing['title']."</a></td>");
 			echo("<td>".date("d-M-y H:i",$finishedListing['date'])."</td>");
-			echo("<td><a href='contactMember.php?user=".$finishedListing['buyer']."'>".$finishedListing['buyer']."</a></td>");
+			
+
+			// Check if the listing sold, if so then setup link to contact buyer
+			if (strcmp($finishedListing['buyer'],"Didn't Sell") == 0) {
+
+				echo("<td>".$finishedListing['buyer']."</td>");
+
+			} else {
+			
+				echo("<td><a href='contactMember.php?user=".$finishedListing['buyer']."'>".$finishedListing['buyer']."</a></td>");
+
+			}
+
 			echo("</tr>");
 
 		}
