4f3a7095582182f0fea56fbe36b502b39b707995
Jack Mitchell <jack@communistcode.co.uk>
2011-12-02 15:04:51 +0000

    update marketplace sidebarlink style

---
+++ includes/marketplaceLinks.inc.php
@@ -0,0 +1,32 @@
+<div class='marketplaceLinkHeadingTopDiv'>Market Place</div>
+
+<ul>
+	<li><a href='index.php'>Home</a></li>	
+	<li><a href='viewListings.php'>View Listings</a></li>
+</ul>
+
+<?php
+		
+	require_once($fullPath."/auction/classes/listingTools.class.php");
+		
+	if (isset($_SESSION['memberLoggedIn'])) {
+
+		echo ("<div class='marketplaceLinkHeading'>Listing Tools</div>");
+
+		$listingTools = new listingTools();
+
+		$linkArray = $listingTools->getSidebarLinks();
+
+		echo ("<ul>");
+
+		foreach ($linkArray as $link) {
+
+			echo("<li><a href='".$link['href'].">".$link['anchor']."</a></li>");
+
+		}
+
+		echo("</ul>");
+
+	}
+
+?>
